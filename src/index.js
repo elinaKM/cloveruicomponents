9644d95f4b1556075e2b5040be67074887bb5380
unknown <elina666@gmail.com>
2019-12-26 22:14:20 +0000

    "Fixing export issue in CloverMarquee[3~[H[D[D[D[D[D[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[[F

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import CloverLinkedButton from './CloverLinkedButton'
 import CloverToggleButton from './CloverToggleButton'
+import CloverMarquee from './CloverMarquee'
 
-export {CloverToggleButton, CloverLinkedButton};
+export {CloverToggleButton, CloverLinkedButton, CloverMarquee};
