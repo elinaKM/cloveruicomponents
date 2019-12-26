02020a5c1323178b9ffbac142b019eb558df7f0b
unknown <elina666@gmail.com>
2019-12-26 22:03:20 +0000

    "Adding CloverMarquee"

---
+++ src/CloverMarquee.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    height: ${props => props.height}; 
+    overflow: hidden;
+    position: relative;
+    display: inline-block;
+`;
+
+const StyledText = styled.h1`
+    color: ${props => props.color};
+    font-size: ${props => props.fontSize};
+
+    margin: 0;
+    transform:translateX(100%);
+    animation: marquee 15s linear infinite;
+
+    @keyframes marquee{
+        0% { transform: translateX(100%) }
+        100% {transform: translateX(-100%) }
+    }
+`;
+
+function CloverMarquee({color, fontSize, children}){
+    return(
+        <StyledDiv height={fontSize}>
+            <StyledText color={color} fontSize={fontSize}>{children}</StyledText>
+        </StyledDiv>
+    );
+}
+
+export default CloverMarquee;
