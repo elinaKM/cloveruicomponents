43dde50471e71e4da1d1bcfb63f33c77210efa24
unknown <elina666@gmail.com>
2020-01-09 11:24:47 +0000

    Logo exported

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import BasicButton from './BasicButton'
 import ToggleButton from './ToggleButton'
 import Marquee from './Marquee'
+import Logo from './Logo'
 
-export {BasicButton, ToggleButton, Marquee};
+export {
+    BasicButton, 
+    ToggleButton, 
+    Marquee,
+    Logo
+};
