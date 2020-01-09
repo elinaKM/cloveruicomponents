93b86d058b64090ee27760e5a68c219692daf966
unknown <elina666@gmail.com>
2020-01-09 11:22:26 +0000

    Adding Logo component

---
+++ src/Logo.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import React from "react";
+import {theme as defaultTheme} from "./Style/theme";
+
+const StyledLogo = styled.h1`
+  font-size: 2em;
+  color: ${props => props.theme.palette.primaryText};
+  text-decoration: none;
+`;
+
+function Logo({theme}){
+    const useTheme = (theme ? theme : defaultTheme);
+    return (
+        <StyledLogo>{`${useTheme.id.logo} ${useTheme.id.name}`}</StyledLogo>
+    );
+}
+
+export default Logo;
